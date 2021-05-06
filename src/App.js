42dec6a84aeed962ab4961241aa01b63911e0723
eds314 <eduardoramirez314@gmail.com>
2021-05-06 17:34:10 -0500

    created a crud task app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
-import Header from './components/Header'
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors appointment",
+      day: "feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at school",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "Feb 6th 3:30pm",
+      reminder: true,
+    },
+  ]);
+
+  //Delete Task
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
-    <div className='container'>
-       <Header />
+    <div className="container">
+      <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No task found"
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,25 @@
-import PropTypes from 'prop-types'
-import Button from './Button'
+import PropTypes from "prop-types";
+import Button from "./Button";
 
-
-const Header = ({title}) => {
+const Header = ({ title }) => {
   const onClick = () => {
-    console.log('Click')
-  }
+    console.log("Click");
+  };
 
   return (
-    <header className='header'>
-     <h1>{title}</h1>
-     <Button color='green' text='Hello' onClick={onClick} />
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
     </header>
-  )
-}
+  );
+};
 
-Header.defaultProps = { 
-  title: 'Task Tracker',
-}
+Header.defaultProps = {
+  title: "Task Tracker",
+};
 
 Header.propTypes = {
-  title:  PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 
-export default Header
+export default Header;
